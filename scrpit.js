b4150e8610bf96b0d0cbfe3a5079e915007721c3
Sivaram vasudevan <58939118+Sivaram01@users.noreply.github.com>
2021-10-15 13:47:55 +0530

    Task-day-8

--- scrpit.js
+++ scrpit.js
@@ -31,38 +31,38 @@ let film = new movie("casino royale" , "Eon Productions" ,"PG13");
 console.log(film);
 console.log(movie3.getPG(movieArray))
 
-//Q-2
+//Q-2  convert uml diagram into class object
 class circle{
-    constructor(radius=1.0,color="red"){
-    this.radius=radius;
-    this.color=color;
-    }
+  constructor(radius=1.0,color="red"){
+  this.radius=radius;
+  this.color=color;
+  }
 getRadius(){
-    console.log(`Radius of circle is${this.radius}`)
+  console.log(`Radius of circle is${this.radius}`)
 }
 setRadius(x){
-    this.radius=x;
-    console.log(`radius of circle is set to ${this.radius}`)
+  this.radius=x;
+  console.log(`radius of circle is set to ${this.radius}`)
 }
 getColor(){
-    console.log(`color of circle is ${this.color}`)
-}
-setColor(x){
-    this.color=x;
-    console.log(`color of circle is set to ${this.color}`)
+  console.log(`color of circle is ${this.color}`)
+} 
+setColor(aqua){
+  this.color=aqua;
+  console.log(`color of circle is set to ${this.color}`)
 }
 getArea(){
-    console.log(`Area of circle is${Math.PI*(this.radius*this.radius)}`)
+  console.log(`Area of circle is${Math.PI*(this.radius*this.radius)}`)
 }
 getCircumference(){
-    console.log(`circumference of circle is ${Math.pi*(this.radius*this.radius*2)}`)
+  console.log(`circumference of circle is ${Math.PI*(this.radius*this.radius*2)}`)
 }
 }
-const circle1=new circle(2,"blue")
+const circle1=new circle(2,"skyblue")
 circle1.getRadius()
 circle1.setRadius(3)
 circle1.getColor()
-circle1.setColor("green")
+circle1.setColor("aqua")
 circle1.getArea()
 circle1.getCircumference()
 
